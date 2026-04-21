ec6f5cdd737e1c35da056099071d68b982d2f399
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-21 12:16:47 +0530

    -day-2 adding migrations and updating database using ef

---
+++ DAY-1/StudentManagementEF/StudentManagementEF/Migrations/20260421055635_AddEmailToStudent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagementEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+        }
+    }
+}
--- DAY-1/StudentManagementEF/StudentManagementEF/Models/Student.cs
+++ DAY-1/StudentManagementEF/StudentManagementEF/Models/Student.cs
@@ -4,13 +4,17 @@ namespace StudentManagementEF.Models
 {
     public class Student
     {
+        [Key]
         public int Id { get; set; }//EF automatically makes this a primary
 
         [Required]
         public string Name { get; set; } = string.Empty;
-
+        [Required]
         [Range(1,100)]
         public int Age { get; set; }
         //designing database table without writing the sql
+        [Required]
+        [MaxLength(100)]
+        public string Email {  get; set; }=string.Empty;//means empty value sam as sttring b=""
     }
 }
