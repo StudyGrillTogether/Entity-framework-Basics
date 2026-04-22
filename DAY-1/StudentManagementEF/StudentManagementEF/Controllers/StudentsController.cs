dde7e0fb3f5b82d2c60202375c3c54253d0a6ba1
Kshitij1ghk <kshitijwk@gmail.com>
2026-04-22 11:25:49 +0530

    DAY-3 ASYNC CHANGES CRUD OPERATIONS , ENTITY STATES CHANGE TRACKING

--- DAY-1/StudentManagementEF/StudentManagementEF/Controllers/StudentsController.cs
+++ DAY-1/StudentManagementEF/StudentManagementEF/Controllers/StudentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentManagementEF.Data;
 using StudentManagementEF.Models;
 
@@ -7,7 +8,7 @@ namespace StudentManagementEF.Controllers
 {
     [ApiController]
     [Route("api/[Controller]")]
-    public class StudentsController:ControllerBase
+    public class StudentsController : ControllerBase
     {
         private readonly AppDbContext _context;
 
@@ -30,10 +31,59 @@ namespace StudentManagementEF.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetStudent()
+        public async Task<IActionResult> GetStudents()//task<IactionResult> is an async return type
         {
-            var students = _context.Students.ToList();
+            var students = await _context.Students.ToListAsync();//await is async execution and tolist async is non blocking
             return Ok(students);
+            //We use ToListAsync() to perform non-blocking database operations,
+            //allowing the application to handle other requests while waiting for data.
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetStudentById(int id)
+        {
+            var student = await _context.Students.FindAsync(id); //fetch by primary key
+            if(student == null)
+            {
+                return NotFound(); //returns 404 
+            }
+
+            return Ok(student);//returns 200 with data
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id,Student updatedStudent)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            student.Name= updatedStudent.Name;
+            student.Age= updatedStudent.Age;
+            student.Email= updatedStudent.Email;
+
+            await _context.SaveChangesAsync();
+            return Ok(student);
+            //We don’t use Update() because EF already tracks the fetched entity and
+            //automatically detects changes, so calling Update() is unnecessary.
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if(student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+            return Ok("Deleted Succesfully");
+            //We fetch the student to ensure it exists and to get a tracked entity that EF can mark as Deleted.
+
+
         }
     }
 }
